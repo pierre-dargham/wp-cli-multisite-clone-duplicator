2c3da833919a918fcf0b367c36144bb3f0a0534c
Pierre DARGHAM <pierre.dargham@globalis-ms.com>
2015-06-08 10:56:57 +0200

    Clean the project

--- multisite-clone-duplicator/multisite-clone-duplicator.php
+++ multisite-clone-duplicator/multisite-clone-duplicator.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * Plugin Name:         MultiSite Clone Duplicator
+ * Plugin URI:          http://wordpress.org/plugins/multisite-clone-duplicator/
+ * Description:         Clones an existing site into a new one in a multisite installation : copies all the posts, settings and files
+ * Author:              Julien OGER, Pierre DARGHAM, GLOBALIS media systems
+ * Author URI:          https://github.com/pierre-dargham/multisite-clone-duplicator
+ *
+ * Version:             1.4.0
+ * Requires at least:   3.5.0
+ * Tested up to:        4.1.2
+ */
 
 // Plugin options
 require_once realpath( dirname( __FILE__ ) ) . '/include/option.php';
